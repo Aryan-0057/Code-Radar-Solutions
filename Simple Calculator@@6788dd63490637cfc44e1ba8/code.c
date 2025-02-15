a7f868b63b873044a95ca44ba5473659520d21c3
Aryan-0057 <aryan0057.becse24@chitkara.edu.in>
2025-02-15 17:34:46 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -5,7 +5,7 @@ int main(){
     char op;
     scanf("%d",&n1);
     scanf("%d",&n2);
-    scanf("%c",&op);
+    scanf(" %c",&op);
     if (op == '+') printf("%d",n1+n2);
     else if (op == '-') printf("%d",n1-n2);
     else if (op == '*') printf("%d",n1*n2);
